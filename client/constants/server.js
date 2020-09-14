377f2f2ec5d3c5986edc8a76a7d321ad3d500f2f
Nancy Wong <nancywong25@gmail.com>
2020-09-14 07:47:31 -0400

    Update Stripe payment serve url

--- client/constants/server.js
+++ client/constants/server.js
@@ -1,4 +1,6 @@
 const PAYMENT_SERVER_URL =
-  process.env.NODE_ENV === 'production' ? '/api/payment' : '/api/payment'
+  process.env.NODE_ENV === 'production'
+    ? 'https://beauty-store-project.herokuapp.com/'
+    : 'http://localhost:8080'
 
 export default PAYMENT_SERVER_URL
